84472f0a238eed271417b352b28c6b919785f8a2
Nuraytncr <nuraytncr@hotmail.com>
2023-12-27 14:14:50 +0100

    Home

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import './Home.css'
+import React from 'react';
+
+function Home() {
+  return (
+    <>
+        <h1>Welkom bij Flavor Maison!</h1>
+        <article> 
+          <p>
+            Jouw culinaire metgezel op de reis naar heerlijke zelfbereide maaltijden! Ontdek een wereld van smaken, inspiratie en gemak direct vanuit je keuken. 
+            Of je nu een doorgewinterde chef-kok bent of net begint aan je culinaire avontuur, Flavor Maison biedt een smaakvolle selectie van recepten, afgestemd op jouw stemming en voorkeuren, 
+            Laat je inspireren, ontdek nieuwe smaken en deel jouw kookpassie met de gemeenschap. Begin vandaag nog met het creëren van onvergetelijke eetervaringen.
+          </p>
+        </article>
+    </>
+  );
+}
+export default Home;
