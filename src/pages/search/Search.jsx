7dcb520865957c8161ac00fca5198b4f335f5aba
Nuraytncr <nuraytncr@hotmail.com>
2024-01-01 19:34:15 +0100

    Improved search usability

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,44 +1,37 @@
 import './Search.css';
 import Input from '../../components/form/Input';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Recipes from '../../components/recipes/Recipes';
+import { addExtraProperties } from '../../components/recipes/Helper';
 
 function Search() {
   const [recipes, setRecipes] = useState([]);
   const [message, setMessage] = useState();
+  const [searchInput, setSearchInput] = useState();
 
-  async function searchRecipes(event) {
+  useEffect(() => {
+    const timeOut = setTimeout(async () => searchRecipes(), 850);
+    return () => clearTimeout(timeOut);
+  }, [searchInput]);
+
+  async function searchRecipes() {
+    if (!searchInput) {
+      return;
+    }
+
     setMessage('Aan het zoeken...');
-    searchRecipes.timeoutId = setTimeout(async () => {
-      try {
-        setRecipes([]);
-        const searchTerm = event.target.value;
-        const edamamURL = import.meta.env.VITE_EDAMAN_URL;
-        const result = await axios.get(`${edamamURL}&random=true&q=${searchTerm}`);
-        const refinedRecipes = addExtraProperties(result.data?.hits);
-        console.log(recipes);
-        setRecipes(refinedRecipes.slice(0, 4));
-      } catch (e) {
-        setMessage('Er ging iets fout. Probeer het later opnieuw.');
-      }
-    }, 1500);
-    console.log(recipes,'test');
+    try {
+      setRecipes([]);
+      const edamamURL = import.meta.env.VITE_EDAMAN_URL;
+      const result = await axios.get(`${edamamURL}&random=true&q=${searchInput}`);
+      const refinedRecipes = addExtraProperties(result.data?.hits.slice(0, 4));
+      setRecipes(refinedRecipes);
+    } catch (e) {
+      setMessage('Er ging iets fout. Probeer het later opnieuw.');
+    }
   }
-  function addExtraProperties(recipes) {
-    const modifiedRecipes = recipes.map((recipeMap) => {
-      const numberArray = [1, 2, 3, 4];
-      const randomNumber = numberArray[Math.floor(Math.random() * numberArray.length)];
-      const selectRecipe = recipeMap.recipe
-      return {
-        ...selectRecipe,
-        difficulty: randomNumber,
-        duration: randomNumber,
-      };
-    });
-    console.log(modifiedRecipes,'modifß')
-    return modifiedRecipes;
-  };
+
   return (
     <>
       <h1>Zoek recepten</h1>
@@ -47,16 +40,16 @@ function Search() {
         name='search'
         id='search-field'
         label='Zoeken naar recepten (naam, ingrediënten, categories)'
-        onChange={searchRecipes}
+        onChangeCallBack={setSearchInput}
         labelStyle='hide'
         errors={[]}
       />
       {
         message && recipes.length === 0 ? <><p>{message}</p></>
           : recipes.length >= 1 ? <Recipes
-            displayRecipes={recipes || []} 
+            displayRecipes={recipes || []}
           />
-          : <p>Vul de zoekfunctie hierboven in.</p>
+            : <p>Vul de zoekfunctie hierboven in.</p>
       }
     </>
   );
